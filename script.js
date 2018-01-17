519c3dc052df0d1507ca9c2f9ade156624b33450
spoodermang <cicconej93@gmail.com>
2018-01-17 09:32:56 -0500

    add pages and js script

---
+++ script.js
@@ -0,0 +1,23 @@
+//get DOM of map button
+//var mapBut = document.getElementsByClassName("mapBut");
+
+//mapBut.addEventListener("onClick", mapRedirect());
+
+
+
+
+function mapRedirect(){
+  location.href = "map.html";
+}
+
+function homeRedirect(){
+  location.href = "index.html";
+}
+
+function aboutRedirect(){
+  location.href = "about.html";
+}
+
+function menuRedirect(){
+  location.href = "menu.html";
+}
